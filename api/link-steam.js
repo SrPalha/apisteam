ed8df9a00fab511854d75a8f2acf7d00a0673d1f
SrPalha <patrikiago09@gmail.com>
2025-05-23 02:40:36 -0300

    up

--- api/link-steam.js
+++ api/link-steam.js
@@ -6,6 +6,14 @@ const supabase = createClient(
 );
 
 export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', 'https://cs.eloninja.com.br');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+
   if (req.method !== 'POST') return res.status(405).send('Method Not Allowed');
   const { user_id, steam_id } = req.body;
 
